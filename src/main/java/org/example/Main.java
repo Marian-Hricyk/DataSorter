79bb2cfaf217105bce34131299462a510242ef8a
Marian-Hricyk <118621179+Marian-Hricyk@users.noreply.github.com>
2024-05-14 18:44:36 +0300

    end result

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,10 +9,10 @@ import java.util.List;
 public class Main {
   public static void main(String[] args) {
     List<LocalDate> unsortedDates = new ArrayList<>();
-    unsortedDates.add(LocalDate.of(2004, 7, 1)); // Липень (без "r")
-    unsortedDates.add(LocalDate.of(2005, 1, 2)); // Січень ("r")
-    unsortedDates.add(LocalDate.of(2007, 1, 1)); // Січень ("r")
-    unsortedDates.add(LocalDate.of(2032, 5, 3)); // Травень (без "r")
+    unsortedDates.add(LocalDate.of(2004, 7, 1));
+    unsortedDates.add(LocalDate.of(2005, 1, 2));
+    unsortedDates.add(LocalDate.of(2007, 1, 1));
+    unsortedDates.add(LocalDate.of(2032, 5, 3));
 
     DateSorter sorter = new DateSorter();
     List<LocalDate> sortedDates = new ArrayList<>(sorter.sortDates(unsortedDates));
